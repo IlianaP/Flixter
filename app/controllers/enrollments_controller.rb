e4a12946800c9ee522342da391da685b59acf822
IlianaP <ip97@cornell.edu>
2016-07-29 14:10:36 +0000

    Setting up payments

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -2,24 +2,25 @@ class EnrollmentsController < ApplicationController
 
 	before_action :authenticate_user! 
 
-	def create 
-		current_user.enrollments.create(course: current_course)  
-
+	def create   
 	# Amount in cents
-    	@amount = 500
+    if current_course.premium? 
+    	@amount = (current_course.cost *100).to_i
 
     	customer = Stripe::Customer.create(
-      	:email => 'example@stripe.com',
+      	:email => current_user.email,
       	:card  => params[:stripeToken]
     	)
 
     	charge = Stripe::Charge.create(
       	:customer    => customer.id,
       	:amount      => @amount,
-      	:description => 'Rails Stripe customer',
+      	:description => 'Flixter Premo Content',
       	:currency    => 'usd'
     	) 
+    end
 
+      current_user.enrollments.create(course: current_course)
     	redirect_to course_path(current_course)
 
   	rescue Stripe::CardError => e
